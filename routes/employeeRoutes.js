a6fba54c82e5c1cfb43f08ce03e4b0d34021734a
BeatyPete <akira941@gmail.com>
2021-01-12 03:08:22 -0600

    finished allemployees call

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -3,11 +3,16 @@ const connection = require('../db/database');
 
 const allEmployees = () => {
     return new Promise((resolve, reject) => {
-      connection.query(`SELECT employees.*, roles.job_title 
-        AS role 
-        FROM employees 
-        LEFT JOIN roles
-        ON employees.role_id = roles.id`, (err, res) => {
+      connection.query(`SELECT employees.*, 
+      roles.job_title AS role,
+      roles.salary AS salary,
+      departments.Dep_Name AS department,
+      b.first_name AS "manager" 
+      FROM employees 
+      LEFT JOIN roles ON employees.role_id = roles.id
+      LEFT JOIN departments ON roles.department_id = departments.id
+      LEFT JOIN employees b ON employees.manager_id = b.id
+      `, (err, res) => {
             if (err) reject(err);
             resolve(res);
         });
